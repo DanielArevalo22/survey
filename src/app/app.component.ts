b2435b896b52b6dadbc5b8f77f311927c96d42af
DanielArevalo22 <danielaresant12@hotmail.com>
2025-09-11 21:29:14 -0500

    SURVEY STRUCTURE UPDATED

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonComponent } from "./components/button/button.component";
+import { CompQuestionsComponent } from "./components/comp-questions/comp-questions.component";
+import { PrincipalQuestionComponent } from "./components/principal-question/principal-question.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PrincipalQuestionComponent, CompQuestionsComponent, ButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
