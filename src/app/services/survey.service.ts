36cac8e48a9e22c12d60f4891354a765c18ab69c
DanielArevalo22 <danielaresant12@hotmail.com>
2025-10-10 14:49:08 -0500

    METHOD SAVE SURVEY CREATED (POST)

--- src/app/services/survey.service.ts
+++ src/app/services/survey.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Survey } from '../models/Survey';
 import { Response } from '../models/Response';
+import { Scores } from '../models/Scores';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,7 @@ import { Response } from '../models/Response';
 export class SurveyService {
 
   private urlGetSurvey = 'http://localhost:6969/api/v1/survey/getSurvey?service=';
+  private urlSaveSurvey = 'http://localhost:6969/api/v1/survey/saveSurvey';
 
   constructor(private http: HttpClient) { }
 
@@ -16,6 +18,10 @@ export class SurveyService {
     return this.http.get<Response<Survey>>(
       this.urlGetSurvey + 'shop'
     );
+  }
+
+  public postSurvey(bodySurvey : Scores){
+    return this.http.post<any>(this.urlSaveSurvey, bodySurvey);
   }
 
 }
