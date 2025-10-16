a83823a7f9db23b0e2c05d065be9f8415d1a38ce
DanielArevalo22 <danielaresant12@hotmail.com>
2025-10-16 10:45:54 -0500

    NEW COMPONENT CREATED 0%

---
+++ src/app/components/completed-window/completed-window.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-completed-window',
+  imports: [],
+  templateUrl: './completed-window.component.html',
+  styleUrl: './completed-window.component.css'
+})
+export class CompletedWindowComponent {
+
+}
